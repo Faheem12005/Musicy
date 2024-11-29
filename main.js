5e4e415253bccfc824429dd3a9749ce70e8523ee
Mohammed <mohdfaheem.1205@gmail.com>
2024-11-29 21:48:56 +0530

    template for main window

--- main.js
+++ main.js
@@ -1,16 +1,27 @@
 const { app, BrowserWindow } = require('electron')
+const path = require('node:path');
 
 const createWindow = () => {
     const win = new BrowserWindow({
+      backgroundColor: '#282828',
+      titleBarStyle: 'hidden',
       width: 800,
-      height: 600
+      height: 600,
+      webPreferences: {
+        preload: path.join(__dirname, 'preload.js')
+      }
     })
-  
+
+    win.once('ready-to-show', () => {
+      win.show();
+    });
     win.loadFile('index.html')
 }
 
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit()
+})
 
-  
 app.whenReady().then(() => {
-  createWindow()
+  createWindow();
 })
